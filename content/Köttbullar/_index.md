798d87a94e6e0863fe953d353c4367992aaea87b
skosulor <nope>
2024-05-01 17:55:16 +0200

    Update

--- content/Köttbullar/_index.md
+++ content/Köttbullar/_index.md
@@ -1,4 +1,4 @@
 +++
-title = 'Kötbullar'
+title = 'Köttbullar'
 date = 2024-04-28T09:55:13+02:00
 +++
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-
 cd "$(dirname "$0")"
 git fetch origin master
 gitStatus=$(git status -sb)
@@ -6,6 +5,8 @@ compare='behind'
 
 if [[ "$gitStatus" == *"$compare"* ]]; then 
     git pull
+    rm -rf public
     hugo
+    rm -rf /var/www
     cp -r public/* /var/www/
 fi
